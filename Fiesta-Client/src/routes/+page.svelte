230d31f6e8432f40cc18e8ccf362679f275d2b27
ReisMiner <r1miner@outlook.com>
2022-10-20 18:02:42 +0200

    hööm

--- Fiesta-Client/src/routes/+page.svelte
+++ Fiesta-Client/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div  class="text-center container mx-auto w-full sm:w-2/3 my-56">
     <Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">We invest in the world’s next teachers!</Heading>
-    <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at <Span highlight class="dark:text-blue-500 text-blue-700">GIBZ FIEŚTA</Span> we focus on schools where technology, innovation, and capital can unlock long-term value in their students.</P>
+    <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at <Span highlight class="text-blue-500 dark:text-blue-500">GIBZ FIEŚTA</Span> we focus on schools where technology, innovation, and capital can unlock long-term value in their students.</P>
     <Button href="/login">Get Started
         <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     </Button>
