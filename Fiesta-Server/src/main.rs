7780969eb4e42f18c60efa33d9916554a3f32c08
ReisMiner <r1miner@outlook.com>
2022-12-16 18:14:12 +0100

    fix cors + done

--- Fiesta-Server/src/main.rs
+++ Fiesta-Server/src/main.rs
@@ -60,10 +60,10 @@ impl Fairing for Cors {
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        // response.set_header(Header::new(
-        //     "Access-Control-Allow-Origin",
-        //     _request.headers().get("origin").next().unwrap(),
-        // ));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Origin",
+            _request.headers().get("origin").next().unwrap(),
+        ));
         response.set_header(Header::new(
             "Access-Control-Allow-Methods",
             "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
